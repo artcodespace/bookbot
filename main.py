866ebd58499947c7c45a5a813ecf7118f0c2475f
alunturner <alun.turner@googlemail.com>
2025-01-18 15:04:03 +0000

    output character count to stdout

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 def count_words(text):
     return len(text.split())
 
+def count_letters(text):
+    letter_count = {}
+    for char in text:
+        lower_case_char = char.lower()
+        if lower_case_char in letter_count:
+            letter_count[lower_case_char] += 1
+        else:
+            letter_count[lower_case_char] = 1
+    return letter_count
+
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
-        print(count_words(file_contents))
+        print(count_letters(file_contents))
 
 main()
 
