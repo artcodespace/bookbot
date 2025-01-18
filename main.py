0da78b554ecdbbc167227652a86799010ac2a41b
alunturner <alun.turner@googlemail.com>
2025-01-18 14:59:46 +0000

    output word count to stdout

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+def count_words(text):
+    return len(text.split())
 
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
-        print(file_contents)
+        print(count_words(file_contents))
 
 main()
 
